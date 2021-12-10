f3610fcda08fd24a28849e1b4419e230d80cee68
Vladimir <vova.prusenok@gmail.com>
2021-12-10 04:21:51 +0300

    made laba8 by Vova and Tanya

---
+++ src/Reader.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Reader {
+
+    public List<String> readFileToList(String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName));
+    }
+}
